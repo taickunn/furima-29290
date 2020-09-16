6a10fb3f4b95e6ded0f507ab27fd1ad808ebcd2d
taickunn <69789079+taickunn@users.noreply.github.com>
2020-09-16 20:41:04 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,32 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    private
+  
+    def item_params
+      params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, :price, :user_id).merge(user_id: current_user.id)
+    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :name, :description, :price, presence: true
+  validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { greater_than: 300}
+  validates :price, numericality: { less_than: 9999999}
+  validates :price, format: {with: /\A[0-9]+\z/}
+  end
+
 end
