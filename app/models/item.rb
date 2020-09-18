76fb888d5dc9ede03f8db78d6c4bcba1a7f2173d
taickunn <69789079+taickunn@users.noreply.github.com>
2020-09-18 15:06:59 +0900

    商品出品機能の修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,12 +10,19 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+
   validates :name, :description, :price, :image
+
   validates :category_id, :condition_id, :postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
+
   validates :name, length: { maximum: 40 }
+
   validates :description, length: { maximum: 1000 } 
+
   validates :price, numericality: { greater_than: 299}
+
   validates :price, numericality: { less_than: 9999999}
+  
   validates :price, format: {with: /\A[0-9]+\z/}
   end
 
