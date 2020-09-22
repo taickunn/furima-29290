c18ff28d4fc0ef8f2cbbbc3a2e61bf2da29c01d3
taickunn <69789079+taickunn@users.noreply.github.com>
2020-09-22 12:43:29 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
-    @items = Item.all.order("created_at DESC")
-    binding.pry
+    @items = Item.all.order("created_at DESC"）
   end
 
   def new
